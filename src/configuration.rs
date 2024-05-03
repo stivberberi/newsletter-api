7fddbf7ad6e7ad9fad3143076310b00bc419bf90
Stiv Berberi <stiv.berberi1@gmail.com>
2024-05-02 20:10:31 -0400

    Add configuration file

---
+++ src/configuration.rs
@@ -0,0 +1,28 @@
+use config;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Add configuration values from a file named 'configuration'. It will look
+    // for any top-level file with an extension that 'config' knows how to
+    // parse: yaml, json, etc.
+    let settings = config::Config::builder()
+        .add_source(config::File::with_name("configuration"))
+        .build()?;
+
+    settings.try_deserialize()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use newsletter_api::configuration::get_configuration;
 use newsletter_api::startup::run;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // set the port to run on
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    println!("Running on 127.0.0.1:8000");
+    let configuration = get_configuration().expect("Failed to read configuration file.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address.clone())?;
+    println!("Running on {}", address);
     run(listener)?.await
 }
